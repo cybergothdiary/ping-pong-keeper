806cdaf309cc7c1e7d3a7b81e82cdb79b149a8c4
Asmodeus <sashaanrad@gmail.com>
2022-09-12 19:02:30 +0300

    score table added, responsive layout, interface fixes & code fixes with inputing players name

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const pNamesInput = document.querySelectorAll('input[type="text"]')
 const winScoreInput = document.querySelector('input[name="winScoreInput"]')
 const players = {
     p1: {
+        name: 'Player 1',
         score: 0,
         btn: document.querySelector('#p1-button'),
         displayScore: score.children[0],
         displayName: names.children[0]
     },
     p2: {
+        name: 'Player 2',
         score: 0,
         btn: document.querySelector('#p2-button'),
         displayScore: score.children[1],
@@ -31,15 +33,27 @@ function updateScore(player) {
     }
 }
 
-function addScore(player) {
-    if (!isOver) {
-        player.score++
-        updateScore(player)
+function updateTable(winner) {
+    const table = document.querySelector('.score-table')
+    const newRow = document.createElement('tr')
+    const newNameP1 = Object.assign(document.createElement('td'), {innerText: `${players.p1.name}`})
+    const newNameP2 = Object.assign(document.createElement('td'), {innerText: `${players.p2.name}`})
+    const newScore = Object.assign(document.createElement('td'), {innerText: `${players.p1.score}:${players.p2.score}`})
+    const newWinner = Object.assign(document.createElement('td'), {innerText: `${winner.name}`})
+    newRow.append(newNameP1, newNameP2, newScore, newWinner)
+    table.append(newRow)
+}
 
-        if (player.score === winScore) {
+function addScore(winner, loser) {
+    if (!isOver) {
+        winner.score++
+        updateScore(winner)
+
+        if (winner.score === winScore) {
             isOver = true
-            player.displayScore.classList.toggle('winner')
-            player.displayScore.classList.toggle('loser')
+            updateTable(winner)
+            winner.displayScore.classList.toggle('winner')
+            loser.displayScore.classList.toggle('loser')
             for (let i of [players.p1, players.p2]) {
                 i.btn.classList.toggle('disabled')
                 i.btn.disabled = true
@@ -48,8 +62,8 @@ function addScore(player) {
     }
 }
 
-players.p1.btn.addEventListener('click', () => addScore(players.p1))
-players.p2.btn.addEventListener('click', () => addScore(players.p2))
+players.p1.btn.addEventListener('click', () => addScore(players.p1, players.p2))
+players.p2.btn.addEventListener('click', () => addScore(players.p2, players.p1))
 resetBtn.addEventListener('click', () => {
     for (let i of [players.p1, players.p2]) {
         i.score = 0
@@ -63,10 +77,27 @@ resetBtn.addEventListener('click', () => {
 
 for (let i = 0; i < 2; i++) { // Names Input
     pNamesInput[i].addEventListener('input', () => {
-        if (i === 0) {
+        if (!pNamesInput[i].value) {
+            if (i === 0) {
+                players.p1.displayName.innerText = 'Player 1'
+                players.p1.name = 'Player 1'
+            } else if (i === 1) {
+                players.p2.displayName.innerText = 'Player 2'
+                players.p2.name = 'Player 2'
+            } else {
+                players.p1.displayName.innerText = 'Player 1'
+                players.p2.displayName.innerText = 'Player 2'
+                players.p1.name = 'Player 1'
+                players.p2.name = 'Player 2'
+            }
+            
+            
+        } else if (i === 0) {
             players.p1.displayName.innerText = pNamesInput[i].value
+            players.p1.name = pNamesInput[i].value
         } else {
             players.p2.displayName.innerText = pNamesInput[i].value
+            players.p2.name = pNamesInput[i].value
         }
     })
 }
